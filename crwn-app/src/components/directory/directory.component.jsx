288af676612501a1c0fe0e19b4e8b11eb70fe0d5
JSCoding365 <jscoding365@gmail.com>
2020-05-03 11:31:51 +0300

    inserted Router, updated menu-items to navigate withRouter HOC

--- crwn-app/src/components/directory/directory.component.jsx
+++ crwn-app/src/components/directory/directory.component.jsx
@@ -47,8 +47,8 @@ class Directory extends React.Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, imageUrl, id, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
         ))}
       </div>
     );
@@ -56,3 +56,13 @@ class Directory extends React.Component {
 }
 
 export default Directory;
+
+// SUBSTITUTED BY "...otherSectionProps"
+// {this.state.sections.map(({ title, imageUrl, id, size, linkUrl }) => (
+//   <MenuItem
+//     key={id}
+//     title={title}
+//     imageUrl={imageUrl}
+//     size={size}
+//     linkUrl={linkUrl}
+//   />
