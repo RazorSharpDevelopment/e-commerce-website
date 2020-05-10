9c2fa369ec31af9ba705302b4912175caa0003a5
JSCoding365 <jscoding365@gmail.com>
2020-05-10 15:43:28 +0300

    included sign up with email

--- crwn-app/src/firebase/firebase.utils.js
+++ crwn-app/src/firebase/firebase.utils.js
@@ -13,6 +13,32 @@ const config = {
   measurementId: "G-H0DT2M8RR1",
 };
 
+export const createUserProfileDocument = async (user, additionalData) => {
+  if (!user) return;
+
+  const userRef = firestore.doc(`users/${user.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = user;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log("error creating user", error.message);
+    }
+  }
+
+  return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
